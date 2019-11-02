d7161e9216b5fdc98ff7ef75d59cb22c5b65fbba
Neil Nie <yongyang.nie@gmail.com>
2019-11-02 11:38:21 -0400

    basic arduino control code written, nothing has been tested.

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/curc/Workspace/mate-rov/src
-# Build directory: /home/curc/Workspace/mate-rov/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("controller")
-subdirs("vision")
--- build/vision/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/curc/Workspace/mate-rov/src/vision
-# Build directory: /home/curc/Workspace/mate-rov/build/vision
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
