ba9e32a0dd7401930eda58dfa77fdcf271a77f20
hada <jonsanria@gmail.com>
2019-11-16 13:14:57 -0500

    version 1 of script now subscribes to ZED camera topics

--- ros/src/vision/scripts/simple-object-tracking/object_tracker.py
+++ ros/src/vision/scripts/simple-object-tracking/object_tracker.py
@@ -10,6 +10,12 @@ import imutils
 import time
 import cv2
 
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+
+
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-p", "--prototxt", required=True,
@@ -28,17 +34,53 @@ ct = CentroidTracker()
 print("[INFO] loading model...")
 net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
 
+"""
 # initialize the video stream and allow the camera sensor to warmup
 print("[INFO] starting video stream...")
 vs = VideoStream(src=0).start()
+"""
 time.sleep(2.0)
+
+depth_frame = None
+current_frame = None
+bridge = CvBridge()
+def rgb_cb( data ):
+	global current_frame
+	try:
+		current_frame = bridge.imgmsg_to_cv2(data, "bgr8")
+		return
+	except CvBridgeError as e :
+		raise e
+	current_frame = None
+
+def depth_cb( data ):
+	global depth_frame
+	try:
+		depth_frame = bridge.imgmsg_to_cv2(data, "bgr8")
+		return
+	except CvBridgeError as e :
+		raise e
+	depth_frame = None
+
+#################################
+######## init rospy #############
+#################################
+rospy.init_node('object_tracker')
+rospy.Subscriber("/zed_node/rgb_raw/image_rect_color", callback=rgb_cb, queue_size=5)
+#rospy.Subscriber("/zed_node/rgb/camera_info", callback=rgb_cb, queue_size=5)
+rospy.Subscriber("/zed_node/depth/depth_registered", callback=rgb_cb, queue_size=5)
+
+
+
+
 
 # loop over the frames from the video stream
 while True:
 	# read the next frame from the video stream and resize it
-	frame = vs.read()
+#	frame = vs.read()
+#	frame = imutils.resize(frame, width=400)
+	frame = current_frame
 	frame = imutils.resize(frame, width=400)
-
 	# if the frame dimensions are None, grab them
 	if W is None or H is None:
 		(H, W) = frame.shape[:2]
@@ -84,10 +126,14 @@ while True:
 	# show the output frame
 	cv2.imshow("Frame", frame)
 	key = cv2.waitKey(1) & 0xFF
+	rospy.spin()
 
 	# if the `q` key was pressed, break from the loop
 	if key == ord("q"):
 		break
+	
+	
+
 
 # do a bit of cleanup
 cv2.destroyAllWindows()
