02972a2ff2b92d76840906124e3bf067a6d5fd94
badinkajink <williamxie00@gmail.com>
2019-11-20 20:06:35 -0500

    umat conversion error fixed, python2 port not working

--- Vision/motion-tracking/motion-tracker.py
+++ Vision/motion-tracking/motion-tracker.py
@@ -2,7 +2,7 @@
 #code used from: https://www.pyimagesearch.com/2015/09/14/ball-tracking-with-opencv/
 #since sample code used to detect a ball, likely will 
 #struggle on side profile views of cap2wa
-""" from imutils.video import VideoStream """
+from imutils.video import VideoStream
 import numpy as np
 import argparse
 import cv2
@@ -14,60 +14,6 @@ import collections
 
 def nothing(x):
     pass
-
-#WEBCAM: Logitech C270 Parameters
-CAM_fov = 60
-CAM_w = 640 #initially 1280
-CAM_h = 480 #initially 720
-
-#will not use args -v, --video
-ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video",
-    help="path to the (optional) video file")
-ap.add_argument("-b", "--buffer", type=int, default=64, 
-    help="max buffer size")
-args = vars(ap.parse_args())
-
-setting = {'hue_min':0, 'hue_max': 180, 'sat_min': 0, 'sat_max': 255, 'val_min': 0, 'val_max': 255}
-setting_file = os.path.join(os.path.expanduser('~'), '.multithresh.json') #for loading specific thresholds later on
-if os.path.exists(setting_file):
-    with open(setting_file, 'r') as f:
-        setting = json.load(f)
-
-#def lower/upper boundaries of the bottle cap, --> "blue"
-#range in HSV values, intialize list of range of hue, saturation, value
-#adjust HERE to filter for different colors
-lower = np.array([setting['hue_min'], setting['sat_min'], setting['val_min']])
-upper = np.array([setting['hue_max'], setting['sat_max'], setting['val_max']])
-
-""" lower = (0, 0, 0)
-upper = (0, 0, 255) """
-pts = collections.deque(maxlen=args["buffer"])
-
-w_name = 'webcam'
-cv2.namedWindow(w_name)
-cv2.namedWindow('track')
-
-cv2.createTrackbar('h_min', 'track', setting['hue_min'], 180, nothing)
-cv2.createTrackbar('s_min', 'track', setting['sat_min'], 255, nothing)
-cv2.createTrackbar('v_min', 'track', setting['val_min'], 255, nothing)
-
-cv2.createTrackbar('h_max', 'track', setting['hue_max'], 180, nothing)
-cv2.createTrackbar('s_max', 'track', setting['sat_max'], 255, nothing)
-cv2.createTrackbar('v_max', 'track', setting['val_max'], 255, nothing)
-
-
-#if no video, refer to webcam-->this is the desired behavior
-if not args.get("video", False):
-    #vs = VideoStream(src=0).start()
-    vs = cv2.VideoCapture(0)
-#if video specified, grab reference to file
-#might hardcode this later to search for index 1 webcam
-else:
-    vs = cv2.VideoCapture(args["video"])
-
-#delay for camera init
-time.sleep(2.0)
 
 #takes input from cv2 sliders to adjust hsv min/max values
 #shows trackbars on different window, 'track'
@@ -88,10 +34,18 @@ def refresh_color(min, max, frame):
     lower[1] = hsv_saturation_min
     lower[2] = hsv_value_min
 
+    hsv_lower = lower[0]*2
+    hsv_upper = upper[0]*2
+    sat_lower = lower
+    sat_upper = upper[1]/2.55
     print("-"*50)
     print(f"Hue: {lower[0]*2:d}-{upper[0]*2:d} deg")
     print(f"Sat: {lower[1]/2.55:.0f}-{upper[1]/2.55:.0f} %")
     print(f"Val: {lower[2]/2.55:.0f}-{upper[2]/2.55:.0f} %")
+"""     print "-"*50
+    print f"Hue: {lower[0]*2:d}-{upper[0]*2:d} deg"
+    print f"Sat: {lower[1]/2.55:.0f}-{upper[1]/2.55:.0f} %"
+    printf"Val: {lower[2]/2.55:.0f}-{upper[2]/2.55:.0f} %" """
 
 #takes raw frame and performs color filtering and noise reduction
 def process(frame):
@@ -158,23 +112,84 @@ def track_targets(mask, frame):
             continue
         
         #otherwise, compute thickness of line and draw connector
-        thickness = int(np.sqrt(args["buffer"] 
+        thickness = int(np.sqrt(10
             / float(i + 1))*2.5)
         cv2.line(frame, pts[i-1], pts[i], (0, 0, 255), thickness)
         cv2.line(mask, pts[i-1], pts[i], (0, 0, 255), thickness)
 
+#WEBCAM: Logitech C270 Parameters
+CAM_fov = 60
+CAM_w = 640 #initially 1280
+CAM_h = 480 #initially 720
 
-while vs.read() is not None: 
-    frame = vs.read()
+#will not use args -v, --video
+""" ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video",
+    help="path to the (optional) video file")
+ap.add_argument("-b", "--buffer", type=int, default=64, 
+    help="max buffer size")
+args = vars(ap.parse_args()) """
+
+setting = {'hue_min':0, 'hue_max': 180, 'sat_min': 0, 'sat_max': 255, 'val_min': 0, 'val_max': 255}
+""" setting_file = os.path.join(os.path.expanduser('~'), '.multithresh.json') #for loading specific thresholds later on
+if os.path.exists(setting_file):
+    with open(setting_file, 'r') as f:
+        setting = json.load(f) """
+
+#def lower/upper boundaries of the bottle cap, --> "blue"
+#range in HSV values, intialize list of range of hue, saturation, value
+#adjust HERE to filter for different colors
+lower = np.array([setting['hue_min'], setting['sat_min'], setting['val_min']])
+upper = np.array([setting['hue_max'], setting['sat_max'], setting['val_max']])
+
+""" lower = (0, 0, 0)
+upper = (0, 0, 255) """
+#pts = collections.deque(maxlen=args["buffer"])
+pts = collections.deque(maxlen=10)
+
+w_name = 'webcam'
+cv2.namedWindow(w_name)
+cv2.namedWindow('track')
+
+cv2.createTrackbar('h_min', 'track', setting['hue_min'], 180, nothing)
+cv2.createTrackbar('s_min', 'track', setting['sat_min'], 255, nothing)
+cv2.createTrackbar('v_min', 'track', setting['val_min'], 255, nothing)
+
+cv2.createTrackbar('h_max', 'track', setting['hue_max'], 180, nothing)
+cv2.createTrackbar('s_max', 'track', setting['sat_max'], 255, nothing)
+cv2.createTrackbar('v_max', 'track', setting['val_max'], 255, nothing)
+
+#if no video, refer to webcam-->this is the desired behavior
+""" if not args.get("video", False):
+    vs = VideoStream(src=0).start() """
+    #vs = cv2.VideoCapture(0)
+#if video specified, grab reference to file
+#might hardcode this later to search for index 1 webcam
+""" else:
+    vs = cv2.VideoCapture(args["video"]) """
+
+#vs = VideoStream(src=0).start()
+vs = cv2.VideoCapture(0)
+
+#delay for camera init
+#time.sleep(2.0)
+if vs.isOpened(): #test if frame opens
+    rval, frame = vs.read()
+else:
+    rval = False
+
+while rval: 
+    rval, frame = vs.read()
     
     #take frame from webcam or video file
-    frame = frame [1] if args.get("video", False) else frame
+    #frame = frame [1] if args.get("video", False) else frame
     """ if frame is None:
         break """
-    #frame = cv2.cvtColor(cv2.UMat(frame), cv2.COLOR_BGR2HSV)
-    #frame  = cv2.resize(frame, (CAM_w, CAM_h))
+    
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    frame  = cv2.resize(frame, (CAM_w, CAM_h))
     mask = process(frame)
-    #mask  = cv2.resize(mask, (CAM_w, CAM_h))
+    mask  = cv2.resize(mask, (CAM_w, CAM_h))
     #shows unfiltered frame
     refresh_color(lower, upper, frame)
     cv2.imshow('track', frame)
@@ -189,9 +204,7 @@ while vs.read() is not None:
     if key_press == 27:
         break
 
-if not args.get("video", False):
-    vs.stop()
-else:
-    vs.release()
+vs.release()
 
 cv2.destroyWindow(w_name)
+
